c19cd257bca2aa136e01d3af6c80dd173bda500a
ysuzuki <ysuzuki@yamanashi.ac.jp>
2024-09-23 11:42:28 +0900

    debug

--- first_app.py
+++ first_app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 from PIL import Image
 
-st.title('アプリ')
-st.caption('test application')
+st.title('Lifestyle-related Disease Improvement Support App')
+st.caption('Diabetic Support')
+
+image = Image.open('oishii3_ojisan.png')
+st.image(image, width=200)
+
+name = st.text_input('name')
+print(name)
+
+submit_btn = st.button('submit')
+cancel_btn = st.button('cancel')
+print(f'submit_btn: {submit_btn}')
+print(f'cancel_btn: {cancel_btn}')
+#
+
 st.subheader('subheader')
 st.text('text text text')
 
@@ -13,8 +26,3 @@ st.title('sample application')
 '''
 
 st.code(code, language='python')
-image = Image.open('oishii3_ojisan.png')
-st.image(image, width=200)
-
-name = st.text_input('name')
-print(name)
