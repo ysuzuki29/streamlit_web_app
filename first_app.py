e5874ad63b809f8142ae4b3325659fab69e6fdf7
ysuzuki <ysuzuki@yamanashi.ac.jp>
2024-10-18 13:26:49 +0900

    SQLite

--- first_app.py
+++ first_app.py
@@ -10,6 +10,40 @@ import time
 from PIL import Image
 import json
 import wave
+import sqlite3
+
+# SQLiteデータベースに接続
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+# テーブルの作成
+c.execute('''CREATE TABLE IF NOT EXISTS users
+             (id INTEGER PRIMARY KEY, mealname TEXT, calorie INTEGER)''')
+ 
+# データの挿入
+if st.button("データを挿入"):
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("hamburger", 100))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("curry", 200))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("gyudon", 300))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("oatmeal", 400))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("gohan", 500))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("pizza", 600))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("udon", 700))
+    c.execute("INSERT INTO users (mealname, calorie) VALUES (?, ?)", ("sushi", 800))
+    conn.commit()
+    st.write("データが挿入されました")
+ 
+# データの取得
+c.execute("SELECT * FROM users")
+rows = c.fetchall()
+ 
+# データの表示
+st.write("ユーザー情報:")
+for row in rows:
+    st.write(row)
+ 
+# 接続を閉じる
+conn.close()
 
 
 st.title('Lifestyle-related Disease Improvement Support App')
@@ -30,10 +64,10 @@ image_sushi = Image.open('nigirizushi_moriawase.png')
 
 st.image(image, width=200)
 
-name = st.text_input('name')
+name = st.text_input('name', placeholder="your name")
 st.write("Hi! ",name)
 
-age = st.text_input('age')
+age = st.text_input('age', placeholder="your age")
 st.write(age)
 
 gender = st.radio(
@@ -47,64 +81,79 @@ if gender==':rainbow[male]':
 else:
     st.image(image_oba, width=200)
 
+height=1
+weight=1
+height = st.text_input('height', placeholder="your height (m)")
+st.write(height)
+
+weight = st.text_input('weight', placeholder="your weight (kg)")
+st.write(weight)
+
+bmi=float(weight)/(float(height)*float(height))
+
+st.write("your BMI is ", bmi)
+
+bmi_class=['x<=18.5', '18.5<x<25', '25<x<30', '30<x<35', '35<x<40', '40<x']
+who_ref=['Underweight', 'Normal range', 'Pre-obese', 'Obese class 1', 'Obese class 2', 'Obese class 3']
+
+# dataframe
+st.write('DataFrame')
+df = pd.DataFrame({
+    'BMI (kg/m^2)': bmi_class,
+    'WHO reference': who_ref
+})
+st.write(df)
+
+st.write("You are in ")
+if bmi <= 18.5:
+    st.write(who_ref[0])
+elif bmi <=25:
+    st.write(who_ref[1])
+elif bmi <=30:
+    st.write(df[1,2])
+elif bmi <=35:
+    st.write(df[1,3])
+elif bmi <=40:
+    st.write(df[1,4])
+else: st.write(df[1,5])
+
+
+#st.dataframe(df.style.highlight_max(axis=0), width=400, height=200)
+
+st.write("What meals did you eat today?")
+
 st.write('examples: hamburger, curry, oatmeal, gyudon, pizza, udon, sushi, gohan')
+
+def showMealPicture(mealname):
+    if mealname=='hamburger':
+        st.image(image_hamburger, width=200)
+    elif mealname=='curry':
+        st.image(image_curry, width=200)
+    elif mealname=='oatmeal':
+        st.image(image_oatmeal, width=200)
+    elif mealname=='gyudon':
+        st.image(image_gyudon, width=200)
+    elif mealname=='pizza':
+        st.image(image_pizza, width=200)
+    elif mealname=='udon':
+        st.image(image_udon, width=200)
+    elif mealname=='sushi':
+        st.image(image_sushi, width=200)
+    else:
+        st.image(image_gohan, width=200)
+  
 
 breakfast = st.text_input('breakfast')
 st.write(breakfast)
-if breakfast=='hamburger':
-    st.image(image_hamburger, width=200)
-elif breakfast=='curry':
-    st.image(image_curry, width=200)
-elif breakfast=='oatmeal':
-    st.image(image_oatmeal, width=200)
-elif breakfast=='gyudon':
-    st.image(image_gyudon, width=200)
-elif breakfast=='pizza':
-    st.image(image_pizza, width=200)
-elif breakfast=='udon':
-    st.image(image_udon, width=200)
-elif breakfast=='sushi':
-    st.image(image_sushi, width=200)
-else:
-    st.image(image_gohan, width=200)
+showMealPicture(breakfast)
 
 lunch = st.text_input('lunch')
 st.write(lunch)
-if lunch=='hamburger':
-    st.image(image_hamburger, width=200)
-elif lunch=='curry':
-    st.image(image_curry, width=200)
-elif lunch=='oatmeal':
-    st.image(image_oatmeal, width=200)
-elif lunch=='gyudon':
-    st.image(image_gyudon, width=200)
-elif lunch=='pizza':
-    st.image(image_pizza, width=200)
-elif lunch=='udon':
-    st.image(image_udon, width=200)
-elif lunch=='sushi':
-    st.image(image_sushi, width=200)
-else:
-    st.image(image_gohan, width=200)
+showMealPicture(lunch)
 
 supper = st.text_input('supper')
 st.write(supper)
-if supper=='hamburger':
-    st.image(image_hamburger, width=200)
-elif supper=='curry':
-    st.image(image_curry, width=200)
-elif supper=='oatmeal':
-    st.image(image_oatmeal, width=200)
-elif supper=='gyudon':
-    st.image(image_gyudon, width=200)
-elif supper=='pizza':
-    st.image(image_pizza, width=200)
-elif supper=='udon':
-    st.image(image_udon, width=200)
-elif supper=='sushi':
-    st.image(image_sushi, width=200)
-else:
-    st.image(image_gohan, width=200)
+showMealPicture(supper)
 
 submit_btn = st.button('submit')
 cancel_btn = st.button('cancel')
@@ -143,7 +192,7 @@ def radar_chart():
        random.randint(0,22)],
     theta=['processing cost','mechanical properties','chemical stability',
            'thermal stability', 'device integration']))
-    fig = px.line_polar(df, r='r', theta='theta', line_close=True)
+    #fig = px.line_polar(df, r='r', theta='theta', line_close=True)
     #audio_placeholder.write(fig)
 
 radar_chart()
